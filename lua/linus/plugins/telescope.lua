58742846a696d6a38740f84da702ea67da479277
Linus Henke <linus.henke@enke5.de>
2023-11-09 17:39:27 +0100

    Added keymaps for telescope.

--- lua/linus/plugins/telescope.lua
+++ lua/linus/plugins/telescope.lua
@@ -1,10 +1,35 @@
-setup, telescope = pcall(require, "telescope.builtin")
+local setup, telescope = pcall(require, "telescope")
 if not setup then
-    print("telescope was not loaded.")
+    print("Could not load telescope")
     return
 end
 
-vim.keymap.set("n", "<leader>ff", telescope.find_files, {})
-vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
-vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
-vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
+local setup, builtin = pcall(require, "telescope.builtin")
+if not setup then
+    print("Could not load telescope.builtin,")
+    return
+end
+
+local setup, actions = pcall(require, "telescope.actions")
+if not setup then
+    print("Could not load telescope.actions")
+    return
+end
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+
+telescope.setup{
+    defaults = {
+        mappings = {
+          i = {
+            ["<C-h>"] = actions.move_selection_next,
+            ["<C-l>"] = actions.move_selection_previous,
+          }
+        }
+      },
+}
+
