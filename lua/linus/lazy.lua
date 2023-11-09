8630d1f7e57410ff52bef9f0323ac97f2a465771
Linus Henke <linus.henke@enke5.de>
2023-11-09 16:25:51 +0100

    Added telesope.

--- lua/linus/lazy.lua
+++ lua/linus/lazy.lua
@@ -20,6 +20,8 @@ end
 
 lazy.setup({
     {"nvim-lua/plenary.nvim"},
+
+    --  Colorscheme
     {
         "morhetz/gruvbox",
         priority = 1000,
@@ -27,13 +29,43 @@ lazy.setup({
             vim.cmd([[colorscheme gruvbox]])
         end,
     },
+
+    --  Toggleterm (Built in Terminal)
     {'akinsho/toggleterm.nvim', version = "*", config = true},
-    {'nvim-lualine/lualine.nvim'},
+
+    --  Tmux-Navigator (Navigate through splits with ctrl)
     {'christoomey/vim-tmux-navigator'},
+
+    --  Vim Maximizer (Maximize splits.)
     {'szw/vim-maximizer'},
+
+    --  Soround (For sorounding movements with characters)
     {"tpope/vim-surround"},
+
+    -- Replace with register (Replace given text with register)
     {"vim-scripts/ReplaceWithRegister"},
+
+    --  Nvim Tree
     {"nvim-tree/nvim-tree.lua"},
+
+    --  LuaLine
     {"nvim-tree/lualine.nvim"},
+
+    --  Dressing (For extra functionality of the input and select function
+    {
+        "stevearc/dressing.nvim",
+        event = "VeryLazy",
+    },
+
+    --  Telescope
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.4",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {"nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+            "nvim-tree/nvim-web-devicons",
+        },
+    },
 })
 
