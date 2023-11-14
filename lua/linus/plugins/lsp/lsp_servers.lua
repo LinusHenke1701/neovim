5559f145d532ca948f6b8641fde9005d9e0fb300
Linus Henke <linus.henke@mci.edu>
2023-11-14 17:24:44 +0100

    Clang now working.

--- lua/linus/plugins/lsp/lsp_servers.lua
+++ lua/linus/plugins/lsp/lsp_servers.lua
@@ -1,9 +1,40 @@
+local status, nvim_lsp = pcall(require, "lspconfig")
+if not status then
+    return
+end
+
 return {
-    clangd = {},
+    clangd = {
+        cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--compile-commands-dir=./build"
+        },
+        init_options = {
+            usePlaceHolders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
+            semanticHighlighting = true,
+        },
+        root_dir = nvim_lsp.util.root_pattern(
+            ".git", "CMakeLists.txt"
+        ),
+        settings = {
+            compileCommandsDirectory = "build",
+        },
+    },
 
     svelte = {},
 
-    cmake = {},
+    cmake = {
+        root_dir = nvim_lsp.util.root_pattern(
+            "CMakeLists.txt"
+        ),
+    },
 
     dockerls = {},
 
