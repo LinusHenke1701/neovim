aa4ef46d147244c649e7e1e3fb2c7c0f24531f0a
Linus Henke <linus.henke@enke5.de>
2024-11-03 11:50:40 +0100

    Disabled unnecessary scripting engines.

--- lua/linus/core/options.lua
+++ lua/linus/core/options.lua
@@ -57,3 +57,11 @@ vim.cmd[[
    au VimLeavePre * lua IncreasePadding()
   augroup END 
 ]]
+
+
+-- Disable unnecessary language support
+vim.g.loaded_python_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_node_provider = 0
