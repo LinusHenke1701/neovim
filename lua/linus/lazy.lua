d6ca4a5e202bac4e9243eb0afa2973b6ee981aff
Linus Henke <linus.henke@enke5.de>
2025-05-26 14:00:41 +0200

    Fixed some errors.

--- lua/linus/lazy.lua
+++ lua/linus/lazy.lua
@@ -88,7 +88,11 @@ lazy.setup({
             "hrsh7th/cmp-path",
         },
     },
-    {"L3MON4D3/LuaSnip"},
+    {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp"
+    },
     {"saadparwaiz1/cmp_luasnip"},
     {"rafamadriz/friendly-snippets"},
 
